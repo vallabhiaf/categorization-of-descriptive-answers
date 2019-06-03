1703f63c184f01427beac8010a9fbb461a507b50
vallabhiaf <vallabh.iaf123@gmail.com>
2019-06-03 13:25:10 +0530

    my first

---
+++ DataSetCollectorFlaskApp/configurations.py
@@ -0,0 +1,8 @@
+config = {
+  "apiKey": "AIzaSyC3tjvvxsqzg5NUPmlvtieKJhbWAdehHGg",
+  "authDomain": "answercheck-d8340.firebaseapp.com",
+  "databaseURL": "https://answercheck-d8340.firebaseio.com",
+  "projectId": "answercheck-d8340",
+  "storageBucket": "answercheck-d8340.appspot.com",
+  "messagingSenderId": "625625074652"
+};
