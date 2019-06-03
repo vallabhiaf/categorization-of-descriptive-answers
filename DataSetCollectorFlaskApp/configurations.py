ef6b31a2597f47ac16d18bc46852b44f692b96c1
vallabhiaf <vallabh.iaf123@gmail.com>
2019-06-03 14:55:03 +0530

    Update configurations.py

--- DataSetCollectorFlaskApp/configurations.py
+++ DataSetCollectorFlaskApp/configurations.py
@@ -1,8 +1,8 @@
 config = {
-  "apiKey": "AIzaSyC3tjvvxsqzg5NUPmlvtieKJhbWAdehHGg",
+  "apiKey": "",
   "authDomain": "answercheck-d8340.firebaseapp.com",
   "databaseURL": "https://answercheck-d8340.firebaseio.com",
   "projectId": "answercheck-d8340",
   "storageBucket": "answercheck-d8340.appspot.com",
-  "messagingSenderId": "625625074652"
+  "messagingSenderId": ""
 };
